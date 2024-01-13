28e1b83bc135dc499ccf11e78f0b145ac400b5f6
Dat Tran <dattthe141510@gmail.com>
2024-01-13 16:00:53 +0700

    cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const bodyParser = require("body-parser")
-const cors = require('cors')
 const app = express()
+const cors = require('cors');
 const corsOptions ={
+    origin:'http://localhost:3000', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
+app.use(cors(corsOptions));
 app.use(cors(corsOptions));
 require("dotenv").config()
 require("./util/mongoose.js")
