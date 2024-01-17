0736710be9f3220647b5e88c0ad552a424bd9c5c
Dat Tran <dattthe141510@gmail.com>
2024-01-18 02:09:28 +0700

    update api bid

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser")
 const app = express()
 const cors = require('cors');
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'https://bidviet-com.vercel.app', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
